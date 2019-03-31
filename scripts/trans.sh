a5d61cdf21e62520bae4ae8153d1b9a3ae19fc02
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2019-03-31 18:49:29 +0200

    Added translation scripts

---
+++ scripts/trans.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#trans -show-original-phonetics n -show-translation-phonetics n -show-prompt-message n -show-dictionary n -show-prompt-message n -show-alternatives n
+
+while read new_line; do
+    #echo "new_line: $new_line"
+    if [[ "$new_line" == "trans.sh pls fix" ]]; then
+        echo "-=====New translation job $(date '+%F %T')=====================-"
+        you="$(cat csgo/csgo_console.log | tac | grep -iEo 'Player:.* - Damage ' | head -n1 | sed 's/^Player: \(.*\) - Damage $/\1/')"
+
+        lines_processed=0
+        while read process_line; do
+           if [[ "$lines_processed" -gt 4 ]]; then
+               break;
+           fi
+           if [[ "$process_line" == "trans.sh pls fix" ]]; then
+               continue
+           fi
+           if [[ "$process_line" == "$you"* ]]; then
+               #echo "you"
+               continue
+           fi
+           lines_to_translate="${lines_to_translate}${process_line}\n"
+           #echo "lines_to_translate rev: $lines_to_translate"
+           lines_processed=$(($lines_processed + 1))
+           #echo "lines_processed: $lines_processed"
+        done < <(tail -n 100 csgo/csgo_console_talk.log | tac)
+
+        lines_to_translate="$(echo -e $lines_to_translate | tac | tail -n +2)"
+        #echo -e "lines_to_translate: $lines_to_translate"
+
+        while read line; do
+
+            if [[ "x" == "x$line" ]]; then echo "ERR line empty"; continue; fi
+
+            player="$(echo "$line" | grep -oE "^.* : " | sed 's/ : //')"
+            message="$(echo "$line" | grep -oE " : .*$" | sed 's/ : //')"
+
+
+            echo "$player : $message"
+            #echo "$message"
+            #echo "$message" | trans -no-ansi -b
+            echo "$message" | trans -no-ansi -show-original-phonetics n -show-translation-phonetics n -show-prompt-message n -show-dictionary n -show-prompt-message n -show-alternatives n -show-original n
+        done < <(echo -e "$lines_to_translate")
+    echo "--------------------------------------------"
+    echo 
+fi
+done < /dev/stdin
